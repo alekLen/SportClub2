ad67dd592cfeb3debb9776db74e2eebb807feb50
Elena <aleklenoch@gmail.com>
2023-10-30 19:15:44 +0200

    #view editTimeT

--- SportClub/Controllers/TimeController.cs
+++ SportClub/Controllers/TimeController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SportClub.BLL.DTO;
 using SportClub.BLL.Interfaces;
+using SportClub.BLL.Services;
 using SportClub.Models;
 using System.Drawing;
+using System.Xml;
 
 namespace SportClub.Controllers
 {
@@ -25,28 +28,35 @@ namespace SportClub.Controllers
         [HttpGet]
         public async Task<IActionResult> AddTimeT()
         {
-            IEnumerable<TimeTDTO> p= await timeService.GetAllTimeTs();
-            List<TimeShow> p1 = new();
-            foreach (var t in p)
-            {
-                TimeShow ts = new()
-                {
-                    Id = t.Id,
-                    Time = t.StartTime + "/" + t.EndTime
-                };
-                p1.Add(ts);
-            }
-
-            ViewData["TimeId"] = new SelectList(p1, "Id", "Time");
+            /* IEnumerable<TimeTDTO> p= await timeService.GetAllTimeTs();
+             IEnumerable<TimeTDTO>p2= p.OrderBy(x => int.Parse(x.StartTime.Split(':')[0]));
+             List<TimeShow> p1 = new();
+             foreach (var t in p2)
+             {
+                 TimeShow ts = new()
+                 {
+                     Id = t.Id,
+                     Time = t.StartTime + "/" + t.EndTime
+                 };
+                 p1.Add(ts);
+             }
+             ViewData["TimeId"] = new SelectList(p1, "Id", "Time");*/
+            await PutTimes();
             return View();
         }
         [HttpPost]
         public async Task<IActionResult> AddTimeT(string Start,string End)
         {
            await timeService.AddTimeT(Start,End);
+            await PutTimes();
+            return View();
+        }
+        public async Task PutTimes()
+        {
             IEnumerable<TimeTDTO> p = await timeService.GetAllTimeTs();
+            IEnumerable<TimeTDTO> p2 = p.OrderBy(x => int.Parse(x.StartTime.Split(':')[0]));
             List<TimeShow> p1 = new();
-            foreach (var t in p)
+            foreach (var t in p2)
             {
                 TimeShow ts = new()
                 {
@@ -55,8 +65,44 @@ namespace SportClub.Controllers
                 };
                 p1.Add(ts);
             }
+            p1.OrderBy(x => x.Time).ToList();
             ViewData["TimeId"] = new SelectList(p1, "Id", "Time");
-            return View();
+        }
+        public async Task<IActionResult> EditTime(int Id)
+        {
+           // HttpContext.Session.SetString("path", Request.Path);
+            TimeTDTO p = await timeService.GetTimeT(Id);
+            if (p != null)
+            {
+                await PutTimes();
+                return View("EditTimeT",p);
+            }         
+            return View("AddTimeT");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditTime( TimeTDTO t)
+        {
+          //  HttpContext.Session.SetString("path", Request.Path);
+            try
+            {
+                TimeTDTO p = await timeService.GetTimeT(t.Id);
+                if (p == null)
+                {
+                    await PutTimes();
+                    return View("AddTimeT");
+                }
+                p.StartTime = t.StartTime;
+                p.EndTime = t.EndTime;
+                await timeService.UpdateTimeT(p);
+                await PutTimes();
+                return View("AddTimeT");
+            }
+            catch
+            {
+                await PutTimes();
+                return View("AddTimeT");
+            }
         }
     }
 }
