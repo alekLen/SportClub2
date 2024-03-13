76df702fffb08fc815e50099eae7e37552735fff
Elena <aleklenoch@gmail.com>
2024-03-13 19:42:52 +0200

    #пагинация при просмотре тренеров и спортзалов на главной странице

--- SportClub/Controllers/HomeController.cs
+++ SportClub/Controllers/HomeController.cs
@@ -20,13 +20,51 @@ namespace SportClub.Controllers
             roomService = r;
         }
 
-        public async Task<IActionResult> Index(string scrollTo)
+        public async Task<IActionResult> Index(string scrollTo, int coachpage = 1, int roompage = 1,
+            bool ispresscoach=false, bool ispressroom = false)
         {
+            if (ispresscoach)
+                scrollTo = "allcoaches";
+            if(ispressroom)
+                scrollTo = "allrooms";
+            int pageSize = 2;
             var p = await coachService.GetAllCoaches();
+            var p1= p.OrderBy(t => t.Id)
+                            .Skip((coachpage - 1) * pageSize)
+                            .Take(pageSize)
+                            .ToList();
             var r= await roomService.GetAllRooms();
+            var r1 = r.OrderBy(t => t.Id)
+                            .Skip((roompage - 1) * pageSize)
+                            .Take(pageSize)
+                            .ToList();
             StartViewModel  start = new StartViewModel();
-            start.coaches=p.ToList();
-            start.rooms=r.ToList();
+            //start.coaches=p.ToList();
+            //start.rooms=r.ToList();
+            start.coaches = p1;
+            start.CurrentPageCoaches = coachpage;
+            int sizeC = 0;
+            if (p.Count() % pageSize == 0)
+            {
+                sizeC = p.Count() / pageSize;
+            }
+            else
+            {
+                sizeC = p.Count() / pageSize + 1;
+            }
+            start.TotalPagesCoaches = sizeC;
+            start.rooms = r1;
+            start.CurrentPageRooms = roompage;
+            int sizeR = 0;
+            if (r.Count() % pageSize == 0)
+            {
+                sizeR = r.Count() / pageSize;
+            }
+            else
+            {
+                sizeR = r.Count() / pageSize + 1;
+            }
+            start.TotalPagesRooms = sizeR;
             ViewBag.ScrollTo = scrollTo;
             return View(start);
         }
--- SportClub/Models/StartViewModel.cs
+++ SportClub/Models/StartViewModel.cs
@@ -5,6 +5,10 @@ namespace SportClub.Models
     public class StartViewModel
     {
         public List<CoachDTO> coaches { get; set; }=new List<CoachDTO>();
+        public int CurrentPageCoaches { get; set; }
+        public int TotalPagesCoaches { get; set; }
         public List<RoomDTO> rooms { get; set; } = new List<RoomDTO>();
+        public int CurrentPageRooms { get; set; }
+        public int TotalPagesRooms { get; set; }
     }
 }
